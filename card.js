f02fd6a976284202203d37baa49c74ab9d8e850f
Sypruskung <namsyprus.paisley@gmail.com>
2014-09-21 16:56:48 +0700

    friends JSON, card object, shuffle cards

--- card.js
+++ card.js
@@ -1,10 +1,60 @@
-$(document).ready(function(){
-   $( "#card1" ).click(function() {
+$(document).ready(function() {
+	
+	$( "#card1" ).click(function() {
   		alert('click1');
 	});
-    int[] cards1 = new int[6];
-    int[] cards2 = new int[6];
-    int[] rancards =  new int[6];
-    int[] allcards =  new int[12];
-    int[] opencards =  new int[12];
+
+	function card(name, imageUrl) {
+		this.name = name;
+		this.image = imageUrl;
+		// alert("creating cart for "+this.name+" with image "+this.image);
+	}
+
+	var cards = [];
+
+	var friends = [
+		{"name":"marc","normal":"marc_normal.jpg", "funny":"marc_funny.jpg"}, 
+		{"name":"jeaw","normal":"marc_normal.jpg", "funny":"marc_funny.jpg"}, 
+		{"name":"korn","normal":"marc_normal.jpg", "funny":"marc_funny.jpg"}, 
+		{"name":"peth","normal":"marc_normal.jpg", "funny":"marc_funny.jpg"}, 
+		{"name":"ping","normal":"marc_normal.jpg", "funny":"marc_funny.jpg"}, 
+		{"name":"gain","normal":"marc_normal.jpg", "funny":"marc_funny.jpg"}
+	];
+
+	jQuery.each(friends, function(index, person) {
+		cards.push(new card(person.name, person.normal));
+		cards.push(new card(person.name, person.funny));
+	});
+
+	jQuery.each(cards, function(index, person) {
+		console.log(person);
+	});
+
+	shuffle(cards);
+
+	console.log("===========shuffle=============");
+
+	jQuery.each(cards, function(index, person) {
+		console.log(person);
+	});
+
 });
+
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex ;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
