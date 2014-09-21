28689b19f796c8ddf68674a83d81637fa97d9e1c
Gain <siraphob@gmail.com>
2014-09-21 15:53:49 -0700

    create card.js

---
+++ card.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+   $( "#card1" ).click(function() {
+  		alert('click1');
+	});
+    int[] cards1 = new int[6];
+    int[] cards2 = new int[6];
+    int[] rancards =  new int[6];
+    int[] allcards =  new int[12];
+    int[] opencards =  new int[12];
+});
