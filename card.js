934aecfd725ae980a435d557d02f23dfe1d7544f
Gain <siraphob@gmail.com>
2014-09-21 17:01:24 -0700

    shuffle script (not use anymore)

--- card.js
+++ card.js
@@ -1,10 +1,22 @@
 $(document).ready(function(){
-   $( "#card1" ).click(function() {
-  		alert('click1');
+
+	var matchno = ['1','2','3','4','5','6','1','2','3','4','5','6'];
+	matchno = shuffle(matchno);
+	var i;
+	for (i = 0; i < matchno.length; i++) { 
+    	//alert("card no."+i+ " | match no."+matchno[i]);
+	}
+   
+    function shuffle(o){ //v1.0
+	    for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+	    return o;
+	};
+    $( ".sponsorFlip" ).click(function() {
+   		var id = this.id;
+   	    var cardno = id.substring(4)-1;
+  		alert("card no."+cardno+ " | match no."+matchno[cardno]);
+  		//idcard = "card"+cardno+1;
+  		$("#card1").html("<img src='http://www.langevin.com/wp-content/uploads/2009/05/smile.png'>");
+  		//$("#img1").attr('src', 'http://www.langevin.com/wp-content/uploads/2009/05/smile.png');
 	});
-    int[] cards1 = new int[6];
-    int[] cards2 = new int[6];
-    int[] rancards =  new int[6];
-    int[] allcards =  new int[12];
-    int[] opencards =  new int[12];
 });
